4ba1428ed21de36c4bc36cfd463bfe8b12ba9590
kevinzhuang01 <kevinhong79928@gmail.com>
2025-08-15 12:54:59 -0700

    fix ApiController

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -1,5 +1,24 @@
 package com.example.demo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api")
 public class ApiController {
-    
+    private final RatelimiterService rateLimiterService;
+
+    public ApiController(RatelimiterService rateLimiterService){
+        this.rateLimiterService = rateLimiterService;
+    }
+
+    @GetMapping("/data")
+    public ResponseEntity<ApiResponse> getData(@RequestParam String userId){
+        if(!rateLimiterService.isAllowed(userId)){
+            ApiResponse response = new ApiResponse("error", "Rate limit exceeded. Wait a max of 60 secs before trying again!");
+            return ResponseEntity.status(429).body(response);
+
+        }
+        ApiResponse response = new ApiResponse("success","Heres your data!");
+        return ResponseEntity.ok(response);
+    }
 }
---
+++ src/main/java/com/example/demo/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory();
+    }
+    @Bean 
+    public RedisTemplate<String,Integer> redisTemplate(){
+        RedisTemplate<String,Integer> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+    
+}
